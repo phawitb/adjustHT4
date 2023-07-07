1a79bd59b6647552e5e8a200e52a8dcb45ee6082
phawitb <96680592+phawitb@users.noreply.github.com>
2023-07-08 00:42:50 +0700

    Update main2.py

--- main2.py
+++ main2.py
@@ -10,29 +10,91 @@ import base64
 repo_owner = "phawitb"
 repo_name = "adjustHT4"
 file_path = "adjust_error.txt"
-access_token = "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"
+access_token = "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"
 
-def update_file(file_content):
-    # file_content = "hellllooooo"  
-    file_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_path}"
-    headers = {"Authorization": f"token {access_token}"}
-    response = requests.get(file_url, headers=headers)
+def delete_file():
+    # API endpoint for deleting a file
+    delete_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_path}"
+    
+    # Headers with authentication
+    headers = {
+        "Authorization": f"token {access_token}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+    
+    # Get the current file details
+    response = requests.get(delete_url, headers=headers)
     file_data = response.json()
+    
+    if "message" in file_data and file_data["message"] == "Not Found":
+        print("File not found.")
+        return
+    
+    # Delete the file
+    delete_payload = {
+        "message": "Delete file",
+        "sha": file_data["sha"]
+    }
+    
+    response = requests.delete(delete_url, json=delete_payload, headers=headers)
+    
+    if response.status_code == 200:
+        print("File deleted successfully.")
+    else:
+        print(f"Error deleting file: {response.content.decode('utf-8')}")
+
+def add_file(file_content):
+    # file_content = "hellllooooo"  
+    # file_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_path}"
+    headers = {"Authorization": f"token {access_token}"}
+    # response = requests.get(file_url, headers=headers)
+    # file_data = response.json()
+
+    # print('file_data',file_data)
 
     new_content = base64.b64encode(file_content.encode('utf-8')).decode('utf-8')
     commit_message = "Update file via web app"
     payload = {
         "message": commit_message,
         "content": new_content,
-        "sha": file_data['sha']
+        # "sha": file_data['sha']
     }
     update_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_path}"
     response = requests.put(update_url, json=payload, headers=headers)
 
-    if response.status_code == 200:
+    print('response.status_code',response.status_code)
+
+    if response.status_code == 201:
         return True
     else:
         return False
+
+    # if response.status_code == 200:
+    #     return True
+    # else:
+    #     return False
+
+# def update_file(file_content):
+#     # file_content = "hellllooooo"  
+#     file_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_path}"
+#     headers = {"Authorization": f"token {access_token}"}
+#     response = requests.get(file_url, headers=headers)
+#     file_data = response.json()
+
+#     new_content = base64.b64encode(file_content.encode('utf-8')).decode('utf-8')
+#     commit_message = "Update file via web app"
+#     payload = {
+#         "message": commit_message,
+#         "content": new_content,
+#         "sha": file_data['sha']
+#     }
+#     update_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_path}"
+#     response = requests.put(update_url, json=payload, headers=headers)
+
+#     if response.status_code == 200:
+#         return True
+#     else:
+#         return False
 
 
 def update(contents,id,adj_temp,adj_humid):
@@ -50,7 +112,10 @@ def update(contents,id,adj_temp,adj_humid):
         file_content += f"{c},{contents[c]['adj_temp']},{contents[c]['adj_humid']}\n"
     file_content = file_content[:-1]
 
-    sta = update_file(file_content)
+    delete_file()
+    sta = add_file(file_content)
+
+    # sta = update_file(file_content)
     return sta
 
 
@@ -70,6 +135,8 @@ def update(contents,id,adj_temp,adj_humid):
 response = requests.get("https://raw.githubusercontent.com/phawitb/adjustHT4/main/adjust_error.txt")
 contents = {}
 for i in response.text.split('\n'):
+    print(i)
+
     if i:
         c = i.split(',')
         contents[c[0]] = {
